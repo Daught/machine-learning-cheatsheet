93820b983a211d825e32ee73b74bd9e68b4f3dd3
SebastianFernandez@Securosys <sebastian.fernandez@securosys.ch>
2024-07-05 15:28:39 +0200

    updated MSD

--- exercises/tpot_diabetes_pipeline_classifier.py
+++ exercises/tpot_diabetes_pipeline_classifier.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline, make_union
 from sklearn.preprocessing import MinMaxScaler
+from tpot.builtins import StackingEstimator
+from xgboost import XGBClassifier
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -12,10 +14,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.9013333333333333
+# Average CV score on the training set was: 0.9111111111111111
 exported_pipeline = make_pipeline(
     MinMaxScaler(),
-    RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.2, min_samples_leaf=8, min_samples_split=4, n_estimators=100)
+    StackingEstimator(estimator=XGBClassifier(learning_rate=0.01, max_depth=2, min_child_weight=15, n_estimators=100, n_jobs=1, subsample=0.1, verbosity=0)),
+    MultinomialNB(alpha=10.0, fit_prior=False)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
